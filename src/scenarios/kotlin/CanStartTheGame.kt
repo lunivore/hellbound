8c65664d39d9b1bee02f2051c721918d4fee34d2
Liz Keogh <liz@lunivore.com>
2019-10-22 17:41:41 +0100

    Using Hamkrest

--- src/scenarios/kotlin/CanStartTheGame.kt
+++ src/scenarios/kotlin/CanStartTheGame.kt
@@ -1,11 +1,13 @@
 package com.lunivore.hellbound
 
 import com.lunivore.stirry.Stirry
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
 import javafx.scene.Group
 import javafx.scene.control.Button
 import javafx.scene.paint.Color
+import javafx.scene.paint.Paint
 import javafx.scene.shape.Rectangle
-import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import tornadofx.*
@@ -25,8 +27,13 @@ class CanStartTheGame {
         val gameGrid = Stirry.findInRoot<Group> {  it.id == "gameGrid" }.value
 
         gameGrid.children.forEach {
-            assertEquals((it as Rectangle).fill.toProperty().get(), Color.BLACK)
+            val color = (it as Rectangle).fill.toProperty().get()!!
+            assertThat(color, equalTo(Color.BLACK as Paint))
         }
+    }
+
+    @Test
+    fun `Pressing a key should start the game`() {
 
     }
 }
